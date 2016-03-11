5266d581b8c1792170ffa4303b65811a19eebf3c
Rainer Gerhards <rger@localhost.localdomain>
2016-03-11 12:33:38 +0100

    system setup for fedora

---
+++ fedora/system_setup.sh
@@ -0,0 +1,4 @@
+sudo dnf install git valgrind autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel
+
+# some extras
+sudo dnf install gvim
