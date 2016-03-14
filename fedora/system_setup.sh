e6af61fec085d54c329ad14e3a0d3764f1984d1d
Rainer Gerhards <rger@localhost.localdomain>
2016-03-14 11:32:40 +0100

    add missing file, add new package installs

--- fedora/system_setup.sh
+++ fedora/system_setup.sh
@@ -1,6 +1,6 @@
 source envvars
 
-sudo dnf -y install clang git valgrind libtool autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel systemd-devel libmongo-client-devel hiredis-devel
+sudo dnf -y install clang git valgrind libtool autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel systemd-devel libmongo-client-devel hiredis-devel qpid-proton-c-devel
 
 # some extras
 sudo dnf -y install gvim
