8249827d543011337e89683ab83ad78597a8d942
Rainer Gerhards <rgerhards@adiscon.com>
2016-03-21 10:02:13 +0100

    add package for faketime command

--- fedora/system_setup.sh
+++ fedora/system_setup.sh
@@ -17,7 +17,7 @@ fi
 source envvars
 
 
-sudo dnf -y install clang git valgrind libtool autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel systemd-devel libmongo-client-devel hiredis-devel qpid-proton-c-devel redhat-rpm-config python-devel elasticsearch
+sudo dnf -y install clang git valgrind libtool autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel systemd-devel libmongo-client-devel hiredis-devel qpid-proton-c-devel redhat-rpm-config python-devel elasticsearch libfaketime
 
 # some extras
 sudo dnf -y install gvim
