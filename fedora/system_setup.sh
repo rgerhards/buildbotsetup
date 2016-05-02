a0c9118a768b8e7aaa259f63d0befc48ec6841d3
Rainer Gerhards <rgerhards@adiscon.com>
2016-05-02 11:24:01 +0200

    we need gdb for stacktrace analysis

--- fedora/system_setup.sh
+++ fedora/system_setup.sh
@@ -18,6 +18,7 @@ source envvars
 
 
 sudo dnf -y install clang git valgrind libtool autoconf automake flex bison python-docutils python-sphinx json-c-devel libuuid-devel libgcrypt-devel zlib-devel openssl-devel libcurl-devel gnutls-devel mysql-devel postgresql-devel libdbi-dbd-mysql libdbi-devel net-snmp-devel systemd-devel libmongo-client-devel hiredis-devel qpid-proton-c-devel redhat-rpm-config python-devel elasticsearch libfaketime \
+	gdb \
 	autoconf-archive
 
 # some extras
